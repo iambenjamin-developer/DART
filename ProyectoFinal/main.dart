7faa2702f81d3b6b10a52467e6bb80c0a6018963
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-02-11 11:16:10 +0800

    leer archivo

--- ProyectoFinal/main.dart
+++ ProyectoFinal/main.dart
@@ -1,7 +1,23 @@
+import 'dart:io';
+
 void main() {
   /*
   Proyecto => abstraccion
   Tarea
   Subtarea
    */
+
+  final textFile = File('./tareas.txt');
+
+  try {
+    if (textFile.existsSync()) {
+      final content = textFile.readAsStringSync();
+      print('''El contenido es:
+      ${content}''');
+    } else {
+      print('El archivo ${textFile.path} no existe');
+    }
+  } catch (e) {
+    print('Ocurrio un error ${e.toString()}');
+  }
 }
